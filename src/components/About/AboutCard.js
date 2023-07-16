83b877f37f633bbb521adbdff740f9105a0acb4c
metin <metinbicaksiz@gmail.com>
2023-07-16 14:18:05 +0300

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { ImPointRight } from 'react-icons/im';
+
+function AboutCard() {
+  return (
+    <Card className='quote-card-view'>
+      <Card.Body>
+        <blockquote className='blockquote mb-0'>
+          <p style={{ textAlign: 'justify' }}>
+            Hi Everyone, I am <span className='purple'>Metin BICAKSIZ </span>
+            from <span className='purple'> Istanbul, Turkiye.</span>
+            <br /> I am a final year student at Istanbul Kultur University
+            Computer Engineering Masters program, working on my thesis about
+            interservice authentication and authorization mechanisms in
+            microservice architecture.
+            <br />
+            Additionally, I am currently a self-employed software developer.
+            <br />
+            <br />
+            Besides coding, some other activities that I love doing!
+          </p>
+          <ul>
+            <li className='about-activity'>
+              <ImPointRight /> Learning
+            </li>
+            <li className='about-activity'>
+              <ImPointRight /> Having time out with friends and family
+            </li>
+            <li className='about-activity'>
+              <ImPointRight /> and of course, time with my daughter
+            </li>
+          </ul>
+
+          <p style={{ color: 'rgb(155 126 172)' }}>
+            "Both Evil and Angel are inside us!" <br /> "We choose which one to
+            let out!"
+          </p>
+          <footer className='blockquote-footer'>Metin</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProjectCard from './ProjectCards';
+import Particle from '../Particle';
+import leaf from '../../Assets/Projects/leaf.png';
+import emotion from '../../Assets/Projects/emotion.png';
+import editor from '../../Assets/Projects/codeEditor.png';
+import chatify from '../../Assets/Projects/chatify.png';
+import suicide from '../../Assets/Projects/suicide.png';
+import bitsOfCode from '../../Assets/Projects/blog.png';
+
+function Projects() {
+  return (
+    <Container fluid className='project-section'>
+      <Particle />
+      <Container>
+        <h1 className='project-heading'>
+          My Recent <strong className='purple'>Works </strong>
+        </h1>
+        <p style={{ color: 'white' }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title='Potr-Pots'
+              description='Implementation of a landing page according to Figma design - Using BEM and SCSS
+              The design 1440px
+              
+              Desktop 1280px
+              
+              Tablet 640px
+              
+              Mobile (> 320px)'
+              ghLink='https://github.com/metinbicaksiz/Potr_Pots'
+              demoLink='https://metinbicaksiz.github.io/Potr_Pots/'
+            />
+          </Col>
+
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title='Layout Dia'
+              description='This is another landing page created using HTML, CSS and Javascript. For the CSS library, I have benefitted the Bulma.
+              It works in the following creen sizes: Large screens 2560px
+              The design 1600px
+              Desktop 1280px
+              Small desktop 1024px
+              Tablet 640px
+              Mobile (> 320px)'
+              ghLink='https://github.com/metinbicaksiz/layout_dia'
+              demoLink='https://metinbicaksiz.github.io/layout_dia/'
+            />
+          </Col>
+
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title='2048 JS Game'
+              description='Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage'
+              ghLink='https://github.com/metinbicaksiz/js_2048_game'
+              demoLink='https://metinbicaksiz.github.io/js_2048_game/'
+            />
+          </Col>
+
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title='Certificate Verification'
+              description='Certificate verification software developed in PHP and MySQL In order to install the application on your server or localhost, just upload the sql database and the files on your host and login as an admin with these credentials: username: admin / password: admin you can create, update and delete news certificates in the database. The admin panel will list all the certificates you have created. You can also use it to help people verify their certificates on the user page by entering their unique certificate numbers.'
+              ghLink='https://github.com/metinbicaksiz/certificate_verification_php-mysql'
+            />
+          </Col>
+
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title='C Library - Libft'
+              description='linft project files for 42 Cursus This is the first project of 42 Cursus about creating my first C library.'
+              ghLink='https://github.com/metinbicaksiz/libft'
+            />
+          </Col>
+
+          {/* <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title='Face Recognition and Emotion Detection'
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink='https://github.com/soumyajit4419/Face_And_Emotion_Detection'
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
