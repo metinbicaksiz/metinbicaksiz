c269bc7b7523de3d713782de6af4fd43b1cda747
metinbicaksiz <72347095+metinbicaksiz@users.noreply.github.com>
2023-07-18 14:06:58 +0300

    Update Projects.js
    last card updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -91,7 +91,7 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title='Imaginer'
-              description="AI supported image generation NEXT.JS app. This app uses replicate.com website for image generation support. Most of the basic Next.js functionalities are included, both SSR and CSR options. Since there is a daily limit of prediction on replicate, there is a rate limiter function, which will only allow you to try 3 times a dat=y from the same device. While giving prompts, you can choose from the templates provided or the images, or you can type your own request. After image generatio, you can also download it produced image."
+              description="AI supported image generation NEXT.JS app. This app uses replicate.com website for image generation support."
               ghLink='https://github.com/metinbicaksiz/imaginer'
               demoLink="https://imaginer.vercel.app"
             />
